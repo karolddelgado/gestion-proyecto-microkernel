7ed45de9a0f60ba3c2c572f1082c299272ffa957
Anderson Vinasco Giraldo <anvigi2001@gmail.com>
2025-03-11 11:42:33 -0500

    create_student_plugin

---
+++ Microkernel_Student/src/main/java/co/edu/unicauca/microkernel_student/Microkernel_Student.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package co.edu.unicauca.microkernel_student;
+
+/**
+ *
+ * @author anvig
+ */
+public class Microkernel_Student {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
