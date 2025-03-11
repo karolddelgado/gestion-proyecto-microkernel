e35e7602c1f97a8e0ee01b685a94dadb2472e17d
Anderson Vinasco Giraldo <anvigi2001@gmail.com>
2025-03-11 11:35:42 -0500

    update classes

---
+++ src/main/java/co/unicauca/microkernel/gestionproyectos/access/UserRepositoryArrayPlugin.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package co.unicauca.microkernel.gestionproyectos.access;
+
+import co.unicauca.microkernel.gestionproyectos.core.domain.entities.Project;
+import co.unicauca.microkernel.gestionproyectos.core.plugin.manager.IProjectRepositoryPlugin;
+import java.util.List;
+
+/**
+ *
+ * @author anvig
+ */
+public class UserRepositoryArrayPlugin implements IProjectRepositoryPlugin{
+    
+    
+
+    @Override
+    public void addProject(Project proyecto) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Project findProjectByTitle(String titulo) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<Project> getProjects() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void initialize() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void shutdown() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
